f370f860a7ba3aaf39a43113f8ca15335d24d360
BoskoVu <57675978+BoskoVu@users.noreply.github.com>
2020-01-15 13:05:37 +0100

    Add files via upload

---
+++ jq1.js
@@ -0,0 +1,20 @@
+$("#prva").on("click",function(){
+
+   
+    $("#tekst").text("Navigacija");
+
+});
+
+$("#druga").on("click",function(){
+
+    
+    $("#tekst").text("Preko");
+
+});
+
+$("#treca").on("click",function(){
+
+    
+    $("#tekst").text("Tabova");
+
+});
